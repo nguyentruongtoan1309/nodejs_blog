553bd032b65905bf0968185cdc34283b9a3032d2
Nguyen Truong Toan <toannguyentruong1309@gmail.com>
2021-08-12 12:28:34 +0700

    Install prettier, husky, lint-staged

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//HTTP logger
+// app.use(morgan('combined'));
+
+//template engine
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        defaultLayout: 'main',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+route(app);
+
+app.listen(port, () => console.log('App listening on port ' + port));
